ba27be10d20a4e4d69baf139b0ecc12a3e817cad
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2022-12-25 22:05:18 +0800

    Update zhenbuka.js

--- zhenbuka.js
+++ zhenbuka.js
@@ -1,1 +1,1 @@
-document.querySelectorAll('body > div.carousel_center.flickity-page > div:nth-child(1)').forEach(function(a){a.remove()})
+document.querySelectorAll('a[href^="/statics/js/ky1219"]').forEach(function(a){a.parentNode.remove()})
